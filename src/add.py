f2927ff4c8926e872c703b62c9d3a69fbfe2b909
felicia3739 <hj20060410@163.com>
2025-10-08 23:09:39 +0800

    Update add.py

--- src/add.py
+++ src/add.py
@@ -4,9 +4,8 @@ def add_from_str(s: str) -> int:
     不要在这个函数里做 input/print。
     提示：用正确的方式捕获字符串s中的两个数，然后对它们进行加法计算。
     """
-    y = 1  # 以这句为例，程序写在这里
-    return y  # 以该变量为例，在这里返回计算结果
-
+    num1, num2 = map(int, s.split())
+    return num1 + num2
 
 if __name__ == "__main__":
     # 程序入口：要求必须使用 input()/print()
